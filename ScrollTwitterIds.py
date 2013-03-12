e896ffbc9dc4128356fca59245be8179ffcff973
Carlos Linares <cadalisa@gmail.com>
2013-03-11 22:16:00 -0430

    Update ScrollTwitterIds.py

--- ScrollTwitterIds.py
+++ ScrollTwitterIds.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import urllib2
 import simplejson as json
 
@@ -6,15 +7,13 @@ for user in range(100):
 	print "User Number: %s"  %user
 	try: 
 		URL = "http://api.twitter.com/1/users/show.json?user_id=%s&include_entities=true" %user
-		f = urllib2.urlopen(URL)
-		list_fancy = json.loads( f.read() )
-
-		print list_fancy['error']
+		reqAPI = urllib2.urlopen(URL)
+		list_fancy = json.loads( reqAPI.read() )
 
 		print "Name:       %s" %list_fancy['name']
 		print "User:       @%s" %list_fancy['screen_name']
 		print "Created at: %s" %list_fancy['created_at']
-		f.close
+		reqAPI.close
 	except:
 		print "I don't know what do"
 	print "====================================================="
